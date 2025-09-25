cdcdee7a52752960dcbe139dc72703553200e85d
Tamás Major <majortamas313@gmail.com>
2025-09-25 09:24:06 +0200

    added linting

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Layout, WelcomeHero } from "./components/layout";
 import { PostList } from "./components/post-list";
 
 function App() {
-  var posts = new Api().postList();
+  const posts = new Api().postList();
   return (
     <Layout>
       <WelcomeHero></WelcomeHero>
